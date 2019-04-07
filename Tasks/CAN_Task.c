17be38012c1925d1b70f70776fd483485d67ff3e
Mahmoud Elgazzar <mahmoud.elgazzar@avelabs.com>
2019-04-07 20:04:48 +0200

    Working Project For Three Nodes , Tested

--- Tasks/CAN_Task.c
+++ Tasks/CAN_Task.c
@@ -13,14 +13,18 @@
 uint8_t State = START_UP_STATE;
 
 /*Variables To Hold Source And Destionation OF Node*/
-uint8_t Source = 3;
-uint8_t Destionation = 4;
+/*Least Source Is Two*/
+uint8_t Source = 4;
+uint8_t Destionation = 5;
 
 /*Variables To Hold Previous Node*/
 #define Previous_Source (Source - 1)
 
 /*Variable To Hold Next Source*/
 #define Next_Source (Source + 1)
+
+/*Define Max Number Of Nodes*/
+#define Max_Nodes 3
 
 void Can_Task_Recive(void)
 {
@@ -48,11 +52,15 @@ void Token_Task()
         if (Switch0_Read() == 1)
         {
             /*Send First Token To The First Node To Inailize System*/
-            Can_Send(Source, Source + 1);
+            Can_Send(Source, Destionation);
+
+            /*Change State To Normal State*/
+            State = NORMAL_STATE;
         }
-        /*Check If Source Node iS Zero*/
+        /*Check If Source Node iS Two*/
         /*This Is Node Number 1*/
-        if (pui8MsgData_Recived[0] == Previous_Source)
+        if ((pui8MsgData_Recived[0] == Previous_Source)
+                && (pui8MsgData_Recived[1] == Source))
         {
             /*Turn Off Led Of init State*/
             led1_off();
@@ -61,6 +69,7 @@ void Token_Task()
             State = NORMAL_STATE;
             led2_on();
             vTaskDelay(100);
+
             /*If Second Button Pressed Change Direction*/
             if (Switch1_Read() == 1)
             {
@@ -68,15 +77,28 @@ void Token_Task()
                 Can_Send(Source, Source - 1);
                 led2_off();
             }
+            /*Send To The Normal Direction*/
             else
             {
                 /*Send token To The Next Node*/
-                Can_Send(Source, Source + 1);
-                led2_off();
+                /*+2 As I Started From Source 2*/
+                if (Destionation < (Max_Nodes + 2))
+                {
+                    Can_Send(Source, Destionation);
+                    led2_off();
+                }
+                else
+                {
+                    /*Send To The Least Address Node*/
+                    /*Destionation Number Two*/
+                    Can_Send(Source, SOURCE_NUM_TWO);
+                    led2_off();
+                }
             }
         }
         /*If Direction Is Changed*/
-        else if (pui8MsgData_Recived[0] == Next_Source)
+        else if ((pui8MsgData_Recived[0] == Next_Source)
+                && ((pui8MsgData_Recived[1]) == Source))
         {
             /*Turn On Led That We Have Recived Token*/
             /*Change State To Normal State*/
@@ -87,7 +109,7 @@ void Token_Task()
             if (Switch1_Read() == 1)
             {
                 /*Send token To The Next Node*/
-                Can_Send(Source, Source + 1 );
+                Can_Send(Source, Source + 1);
                 led2_off();
             }
             else
